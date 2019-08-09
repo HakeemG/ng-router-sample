6b16ffcc6a257210d30652388fd87267e0a13040
Hakeem Giydan <hgiydan@gmail.com>
2019-08-09 12:14:51 -0700

    Define a Wildcard route
    Signed-off-by: Hakeem Giydan <hgiydan@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'crisis-center', component: CrisisListComponent },
   { path: 'heroes', component: HeroListComponent },
+  { path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
