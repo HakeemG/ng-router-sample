eda2640ac9a6e82443da962b631a0979397a5520
Hakeem Giydan <hgiydan@gmail.com>
2019-08-12 06:50:57 -0700

    adding message service
    Signed-off-by: Hakeem Giydan <hgiydan@gmail.com>

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { MessageService } from '../message.service';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -11,13 +13,23 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService,
+  ) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    console.log(`HeroService: ${message}`);
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getHeroes(): Observable<Hero[]> {
+    this.log('fetched heroes');
     return of(HEROES);
   }
 
   getHero(id: number | string) {
+    this.log(`fetched hero id=${id}`);
     return this.getHeroes()
       .pipe(
         map((heroes: Hero[]) => heroes.find(hero => hero.id === +id))
